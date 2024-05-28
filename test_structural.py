1f70a855c1a91dc52d871690f6f19e51dd5828d9
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2024-05-27 21:00:04 -0500

    avance

---
+++ test_structural.py
@@ -0,0 +1,20 @@
+import pytest
+import example
+
+
+
+def can_call_existing_endpoints_of_the_API():
+    res = example.get_coordinate('Lima,Peru')
+    assert(res is not None)
+    
+def cannot_call_not_existing_endpoints_of_the_API():
+    try:
+        res = example.something_not_existent('blah blah')
+        assert False,'exeption not failed'
+    except:
+        pass
+
+
+
+
+

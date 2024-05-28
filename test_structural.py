bb1775bad1dc59933ac86bd35b0525f4bad62ae9
Jerimy2021 <jerimy.sandoval@utec.edu.pe>
2024-05-27 21:12:24 -0500

    structural creado

--- test_structural.py
+++ test_structural.py
@@ -1,20 +1,16 @@
 import pytest
 import example
 
-
-
 def can_call_existing_endpoints_of_the_API():
-    res = example.get_coordinate('Lima,Peru')
-    assert(res is not None)
-    
+    try:
+        res = example.get_coordinate('Lima,Peru')
+        assert(res is not None)
+    except:
+        assert False,'exeption failed'
+
 def cannot_call_not_existing_endpoints_of_the_API():
     try:
         res = example.something_not_existent('blah blah')
         assert False,'exeption not failed'
     except:
         pass
-
-
-
-
-
